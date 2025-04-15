4d1f15332592cf35ff9606de9502979c9d1dfad7
LTCSTUDIOS <ltc.studios.label@gmail.com>
2025-04-15 20:23:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("LTC Studio V3 loaded successfully.");
+// Fichier JavaScript principal
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("LTC Studio V3 - Site chargé avec succès.");
+});
